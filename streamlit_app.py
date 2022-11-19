a8f5108b5bb00ddd9ca9128209e5b431023d0d82
PrasunMajumder <118586285+prasuntrek@users.noreply.github.com>
2022-11-18 23:52:12 -0600

    Update streamlit_app.py
    Added lots of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 streamlit.title("Prasun New Data App for Dining")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach, Smoothie")
-streamlit.text("Free Range Egg")
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('Mango Smoothie')
+streamlit.text('Apple Shake')
