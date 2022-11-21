ea4d717f8c30bff8bc043d151b8bb98ff12253fa
PrasunMajumder <118586285+prasuntrek@users.noreply.github.com>
2022-11-21 16:57:27 -0600

    Update streamlit_app.py
    Adding insert statement for fruit load list table

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What Fruit would you like to add?')
 streamlit.write('Thanks for adding:  ', add_my_fruit)
 
+my_cur2 = my_cnx.cursor()
+my_cur2.execute("insert into fruit_load_list values ('from streamlit')")
