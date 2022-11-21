07f7399b08fcfcb29814655cb9cf0626078319ab
PrasunMajumder <118586285+prasuntrek@users.noreply.github.com>
2022-11-21 16:32:09 -0600

    Update streamlit_app.py
    Added snowflake connector import code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,7 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
+# Adding snowflake connector code
+import snowflake.connector
